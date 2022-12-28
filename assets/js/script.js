ac16aae5680eeb048f0dc6322bd072a68ceb3d07
WJFil <greg.kitchen7@gmail.com>
2022-12-27 17:52:10 -0700

    12-27 changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,7 +49,7 @@ var questions= [
     {
         question: 'Commonly used data types do not include __________.',
         answers: ['strings','booleans','alerts','numbers'],
-        correct: 3
+        correct: 'alerts',
     },
 
     {
@@ -92,9 +92,9 @@ var questions= [
 
 var currentQuest =0;
 var ans1 = questions[currentQuest].answers[0];
-var ans2 = questions[currentQuest].answers[1].text;
-var ans3 = questions[currentQuest].answers[2].text;
-var ans4 = questions[currentQuest].answers[3].text;
+var ans2 = questions[currentQuest].answers[1];
+var ans3 = questions[currentQuest].answers[2];
+var ans4 = questions[currentQuest].answers[3];
 var currentQuestText=questions[currentQuest].question;
 var prntQuestText=document.getElementById('prntQuestion').innerHTML = currentQuestText;
 var prntAns1Text=document.getElementById('ans1').innerHTML = ans1;
@@ -102,7 +102,7 @@ var prntAns2Text=document.getElementById('ans2').innerHTML = ans2;
 var prntAns3Text=document.getElementById('ans3').innerHTML = ans3;
 var prntAns4Text=document.getElementById('ans4').innerHTML = ans4;
 
-console.log(prntAns1Text)
+
 
 
 
@@ -114,41 +114,59 @@ var ans4Button = document.getElementById('ans4')
 
 
 
-ans1Button.addEventListener('click', nextQuestion);
+
 ans1Button.addEventListener('click', ansSelected);
+ans2Button.addEventListener('click', ansSelected);
+ans3Button.addEventListener('click', ansSelected);
+ans4Button.addEventListener('click', ansSelected);
+ans1Button.addEventListener('click', nextQuestion);
 ans2Button.addEventListener('click', nextQuestion);
 ans3Button.addEventListener('click', nextQuestion);
 ans4Button.addEventListener('click', nextQuestion);
 
 function ansSelected(event) {
    
-    var id=event.target; 
-    console.log(prntAns1Text)
+    var id=event.target.id;
+    var result= document.getElementById(id).innerHTML;
+    console.log(result)
+
+     
+  
 }
 
-console.log(ansSelected)
+var userAns = ansSelected;
 
-
+console.log(userAns)
 
     
 function nextQuestion() {
     
-    var correct = true;
-    if (correct===true){
+    var correct = questions[currentQuest].correct;
+    var correctAns = true;
+    var userAns = ansSelected;
+
+    console.log(userAns)
+
+
+    
+    if (correctAns===true){
         document.getElementById('response-text').innerHTML='Correct!'
     } else {'Wrong!'
 };   
-    response.id='response-show'
-    currentQuest++;
+    response.id='response-show';
     console.log(currentQuest);
+    console.log(currentQuest<questions.length);
+    if (currentQuest<questions.length-1) {
+    currentQuest++;
     document.getElementById('prntQuestion').innerHTML = questions[currentQuest].question;
     document.getElementById('ans1').innerHTML = questions[currentQuest].answers[0].text;
     document.getElementById('ans2').innerHTML = questions[currentQuest].answers[1].text;
     document.getElementById('ans3').innerHTML = questions[currentQuest].answers[2].text;
     document.getElementById('ans4').innerHTML = questions[currentQuest].answers[3].text;
- 
+    } else {endQuiz()};
     
 }
+
   
 
 
