73ac320b82c086b8ccaccc8d1f793e007f23b518
WJFil <greg.kitchen7@gmail.com>
2023-01-04 15:12:10 -0700

    fix answer check to work correctly.  Also store scores and initials as array to localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,172 +1,196 @@
 var startContent = document.getElementById('start');
 var quizContent = document.getElementById('quiz');
-var response =document.getElementById('response');
+var response = document.getElementById('response');
 var quizEnd = document.getElementById('quiz-end')
-var timerEl=document.getElementById('timer');
-var secondsLeft = 10;
+var timerEl = document.getElementById('timer');
+var secondsLeft = 60;
+var score = 0;
 
 
-function setTime(){
+
+function setTime() {
     // Sets interval in variable
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timerEl.innerHTML = "Time: "+ secondsLeft;
-  
-      if(secondsLeft === 0) {
-        // Stops execution of action at set interval
-        clearInterval(timerInterval);
-        // Calls function to create and append image
-        endQuiz();
-      }
-  
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timerEl.innerHTML = "Time: " + secondsLeft;
+
+        if (secondsLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+            // Calls function to create and append image
+            endQuiz();
+        }
+
     }, 1000);
-  }
+}
 
 
 function endQuiz() {
-    quizContent.id='quiz'
-    quizEnd.id='quiz-end-show'
- }
+    quizContent.id = 'quiz'
+    quizEnd.id = 'quiz-end-show'
+}
 var startBtn = document.getElementById('start-btn');
 
 function startQuiz() {
     setTime();
     startContent.style.display = 'none';
     quizContent.id = 'quiz-show'
-    
+
 
 }
 
 
 
-startBtn.addEventListener('click', startQuiz);
-
-
-
-
 //list of questions for the quiz
-var questions= [
+var questions = [
     {
         question: 'Commonly used data types do not include __________.',
-        answers: ['strings','booleans','alerts','numbers'],
+        answers: ['strings', 'booleans', 'alerts', 'numbers'],
         correct: 'alerts',
     },
 
     {
         question: 'The condition of an if/else statement is enclosed with __________.',
-        answers: [
-            {text: 'quotes', correct: false}, 
-            {text: 'curly brackets', correct: false}, 
-            {text: 'parenthesis', correct: true}, 
-            {text: 'square brackets', correct: false}, 
-        ]
+        answers: ['quotes', 'curly brackets', 'parenthesis', 'square brackets'],
+        correct: 'parenthesis',
     },
     {
         question: 'Arrays in JavaScript can be used to store __________.',
-        answers: [
-            {text: 'numbers and strings', correct: false}, 
-            {text: 'other arrays', correct: false}, 
-            {text: 'booleans', correct: false}, 
-            {text: 'all of the above', correct: true}, 
-        ]
+        answers: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+        correct: 'all of the above',
+
     },
     {
         question: 'String values must be enclosed within __________ when being assigned to variables.',
-        answers: [
-            {text: 'commas', correct: false}, 
-            {text: 'curly brackets', correct: false}, 
-            {text: 'quotes', correct: true}, 
-            {text: 'parenthesis', correct: false}, 
-        ]
+        answers: ['commas', 'curly brackets', 'quotes', 'parenthesis'],
+        correct: 'quotes',
+
     },
     {
         question: 'A very useful tool used during development and debugging for printing content to the debugger is __________.',
-        answers: [
-            {text: 'Javascript', correct: false}, 
-            {text: 'terminal/bash', correct: false}, 
-            {text: 'for loops', correct: false}, 
-            {text: 'console.log', correct: true}, 
-        ]
+        answers: ['Javascript', 'terminal/bash', 'for loops', 'console.log'],
+        correct: 'console.log',
+
     }
 ]
 
-var currentQuest =0;
-var ans1 = questions[currentQuest].answers[0];
-var ans2 = questions[currentQuest].answers[1];
-var ans3 = questions[currentQuest].answers[2];
-var ans4 = questions[currentQuest].answers[3];
-var currentQuestText=questions[currentQuest].question;
-var prntQuestText=document.getElementById('prntQuestion').innerHTML = currentQuestText;
-var prntAns1Text=document.getElementById('ans1').innerHTML = ans1;
-var prntAns2Text=document.getElementById('ans2').innerHTML = ans2;
-var prntAns3Text=document.getElementById('ans3').innerHTML = ans3;
-var prntAns4Text=document.getElementById('ans4').innerHTML = ans4;
+var currentQuest = 0;
 
 
+function displayQuestion() {
+    var currentQuestText = questions[currentQuest].question;
+    document.getElementById('prntQuestion').innerHTML = currentQuestText;
 
+    var ans1Button = document.getElementById('ans1')
+    var ans2Button = document.getElementById('ans2')
+    var ans3Button = document.getElementById('ans3')
+    var ans4Button = document.getElementById('ans4')
 
+    var ans1 = questions[currentQuest].answers[0];
+    var ans2 = questions[currentQuest].answers[1];
+    var ans3 = questions[currentQuest].answers[2];
+    var ans4 = questions[currentQuest].answers[3];
 
+    ans1Button.innerHTML = ans1;
+    ans2Button.innerHTML = ans2;
+    ans3Button.innerHTML = ans3;
+    ans4Button.innerHTML = ans4;
 
-var ans1Button = document.getElementById('ans1')
-var ans2Button = document.getElementById('ans2')
-var ans3Button = document.getElementById('ans3')
-var ans4Button = document.getElementById('ans4')
+    var answerBtn = document.querySelectorAll(".answerBtn")
 
+    answerBtn.forEach(function (btn) {
+        console.log(btn)
+        btn.addEventListener("click", nextQuestion)
 
+        //  btn.onclick = nextQuestion;
 
+    })
 
-ans1Button.addEventListener('click', ansSelected);
-ans2Button.addEventListener('click', ansSelected);
-ans3Button.addEventListener('click', ansSelected);
-ans4Button.addEventListener('click', ansSelected);
-ans1Button.addEventListener('click', nextQuestion);
-ans2Button.addEventListener('click', nextQuestion);
-ans3Button.addEventListener('click', nextQuestion);
-ans4Button.addEventListener('click', nextQuestion);
+    // ans1Button.addEventListener('click', () => {
+    //     ansSelected = ans1
+    //     console.log(ansSelected)
+    //     nextQuestion();
+    // });
+    // ans2Button.addEventListener('click', async () => {
+    //     ansSelected = await ans2
+    //     console.log(ansSelected)
+    //     nextQuestion();
+    // });
+    // ans3Button.addEventListener('click', () => { ansSelected = ans3 });
+    // ans4Button.addEventListener('click', () => { ansSelected = ans4 });
 
-function ansSelected(event) {
-   
-    var id=event.target.id;
-    var result= document.getElementById(id).innerHTML;
-    console.log(result)
+    // ans1Button.addEventListener('click', nextQuestion);
+    // ans2Button.addEventListener('click', nextQuestion);
+    // ans3Button.addEventListener('click', nextQuestion);
+    // ans4Button.addEventListener('click', nextQuestion);
 
-     
-  
 }
 
-var userAns = ansSelected;
-
-console.log(userAns)
-
-    
 function nextQuestion() {
-    
-    var correct = questions[currentQuest].correct;
-    var correctAns = true;
-    var userAns = ansSelected;
 
-    console.log(userAns)
+    var ansCorrect = questions[currentQuest].correct;
+    var ansSelected = this.textContent
+    console.log(this.textContent)
 
 
-    
-    if (correctAns===true){
-        document.getElementById('response-text').innerHTML='Correct!'
-    } else {'Wrong!'
-};   
-    response.id='response-show';
     console.log(currentQuest);
-    console.log(currentQuest<questions.length);
-    if (currentQuest<questions.length-1) {
-    currentQuest++;
-    document.getElementById('prntQuestion').innerHTML = questions[currentQuest].question;
-    document.getElementById('ans1').innerHTML = questions[currentQuest].answers[0].text;
-    document.getElementById('ans2').innerHTML = questions[currentQuest].answers[1].text;
-    document.getElementById('ans3').innerHTML = questions[currentQuest].answers[2].text;
-    document.getElementById('ans4').innerHTML = questions[currentQuest].answers[3].text;
-    } else {endQuiz()};
-    
+    console.log(ansSelected);
+    console.log(ansCorrect);
+
+    /*console.log(ans1);
+    console.log(ans2);
+    console.log(ans3);
+    console.log(ans4);
+
+    */
+
+
+
+
+    if (ansCorrect === ansSelected) {
+        document.getElementById('response-text').innerHTML = 'Correct!';
+        score++;
+        document.getElementById('score').innerHTML = score;
+    } else {
+        document.getElementById('response-text').innerHTML = 'Wrong!';
+    };
+    response.id = 'response-show';
+
+    if (currentQuest < questions.length - 1) {
+        currentQuest++;
+        displayQuestion();
+        // document.getElementById('prntQuestion').innerHTML = questions[currentQuest].question;
+        // document.getElementById('ans1').innerHTML = questions[currentQuest].answers[0];
+        // document.getElementById('ans2').innerHTML = questions[currentQuest].answers[1];
+        // document.getElementById('ans3').innerHTML = questions[currentQuest].answers[2];
+        // document.getElementById('ans4').innerHTML = questions[currentQuest].answers[3];
+
+    } else { endQuiz() };
+
+
+
 }
 
-  
+var subButton = document.getElementById('submit');
+var initialsInput = document.getElementById('initials');
+var highscoreArr = JSON.parse(localStorage.getItem('savedResult')) || [];
 
 
+startBtn.addEventListener('click', function () {
+    startQuiz();
+    displayQuestion();
+});
+
+subButton.addEventListener('click', function (event) {
+
+
+
+    var savedResult = {
+        initials: initialsInput.value,
+        finalScore: score
+    }
+
+    highscoreArr.push(savedResult)
+
+    localStorage.setItem('savedResult', JSON.stringify(highscoreArr));
+})
